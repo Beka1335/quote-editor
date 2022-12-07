f61ab9ebe7b47b36f3087266c503bb890e2dce97
Beka Arishvili <b.arishvili@sloboda-studio.com>
2022-12-07 12:33:40 +0400

    chapter seven

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,38 @@ class ApplicationController < ActionController::Base
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, unless: :devise_controller?
 
+  def create
+    @quote = current_company.quotes.build(quote_params)
+
+    if @quote.save
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @quote.update(quote_params)
+      redirect_to quotes_path, notice: "Quote was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @quote.destroy
+
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream
+    end
+  end
+
   private
-  
+
   def set_quote
     # We must use current_company.quotes here instead of Quote
     # for security reasons
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,14 +15,14 @@ class QuotesController < ApplicationController
 
   def create
     @quote = current_company.quotes.build(quote_params)
-
+  
     if @quote.save
       respond_to do |format|
-        format.html { redirect_to quotes_path, notice: 'Quote was successfully created.' }
-        format.turbo_stream
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
       end
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
 
@@ -30,19 +30,21 @@ class QuotesController < ApplicationController
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: 'Quote was successfully updated.'
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
+      end
     else
-      # Add `status: :unprocessable_entity` here
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @quote.destroy
-
+  
     respond_to do |format|
-      format.html { redirect_to quotes_path, notice: 'Quote was successfully destroyed.' }
-      format.turbo_stream
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed." }
     end
   end
 

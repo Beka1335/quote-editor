1192c914b90738c64c498caea3bba1535514df47
Beka Arishvili <b.arishvili@sloboda-studio.com>
2022-12-08 16:22:36 +0400

    fixed some bugs

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -21,9 +21,7 @@ class QuotesController < ApplicationController
     if @quote.save
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: 'Quote was successfully created.' }
-        format.turbo_stream do
-          flash.now[:notice] = 'Quote was successfully created.'
-        end
+        format.turbo_stream { flash.now[:notice] = 'Quote was successfully created.' }
       end
     else
       render :new

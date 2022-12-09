cde64bd303d607b732ad440dd1a7e33a47792e95
Beka Arishvili <b.arishvili@sloboda-studio.com>
2022-12-09 14:55:17 +0400

    initial setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << 'quote-editor-production-f1ae.up.railway.app'
+  config.hosts << 'quote-editor-production-abf6.up.railway.app'
   # Code is not reloaded between requests.
   config.cache_classes = true
 

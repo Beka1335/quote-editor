9866dc64d5659749ab61674704094d5b4e9e48fd
Beka Arishvili <b.arishvili@sloboda-studio.com>
2022-12-09 17:34:32 +0400

    initial setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << 'quote-editor-production-31f4.up.railway.app'
+  config.cache_store = :redis_cache_store, { url: "redis://localhost:6379/0" }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
